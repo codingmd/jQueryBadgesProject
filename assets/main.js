56bdc4d5b0b54230bad7bfa4b61fa049a6051b85
MD <codepro.md@gmail.com>
2017-02-23 21:37:39 -0800

    Update main.js w/ajax json

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,32 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/coding_md.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      
+      var $courses = $('#response.courses.completed');
+      var $badges = $('#badges');
+      var $url = $('#url');
+
+      courses.forEach(function(course) {
+
+      	var $course = $('<div />', {
+      		'class': 'course'
+      	}).appendTo($badges);
+
+      	$('<h3 />', {
+      		text: course.title
+      	}).appendTo($course);
+
+      	$('<img src="$badges" />').appendTo($course);
+
+      	$('<a href="$url" target="_blank" class="btn btn-primary"></a>', {
+      		text: "See course"
+      	}).appendTo($course);
+      })
+
+    }
+  });
 
 });

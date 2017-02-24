394f6f4a75a8d78ce6d48243b5f11940eb7c8810
MD <codepro.md@gmail.com>
2017-02-24 00:09:07 -0800

    Prettify project: main.js,main.css,index.html

--- assets/main.js
+++ assets/main.js
@@ -5,12 +5,15 @@ $(function() {
     dataType: 'jsonp',
     async:false,
     success: function(response) {
-    	console.log('response', response.courses);
+    	console.log('response', response.courses.badges);
       
       courses = response.courses.completed;
       progress = response.courses.in_progress;
+      awards = response.badges;
+
       var $badges = $('#badges');
       var $progress = $('#progress');
+      var $awards = $('#awards');
       
 
       courses.forEach(function(course) {
@@ -56,7 +59,28 @@ $(function() {
       		text: 'See course'
       	}).appendTo($course);
       })
-    }
-  });
 
+      awards.forEach(function(course) {
+
+      	var $course = $('<div />', {
+      		'class': 'course'
+      	}).appendTo($awards);
+
+      	$('<h3 />', {
+      		text: course.name
+      	}).appendTo($course);
+
+      	$('<img />', {
+      		src: course.badge 
+      	}).appendTo($course);
+
+      	$('<a />', {
+      		href: course.course_url,
+      		target: '_blank',
+      		'class': 'btn btn-primary',
+      		text: 'See course'
+      	}).appendTo($course);
+     })
+  }
+})
 });

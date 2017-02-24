99e4c22f583c3ace019a63a23335b67b1d500f60
MD <codepro.md@gmail.com>
2017-02-23 22:43:09 -0800

    Update main.js index.html - It works!

--- assets/main.js
+++ assets/main.js
@@ -3,17 +3,21 @@ $(function() {
   $.ajax({
     url: 'https://www.codeschool.com/users/coding_md.json',
     dataType: 'jsonp',
+    async:false,
     success: function(response) {
+    	console.log('response', response.courses);
       
       courses = response.courses.completed;
-      var $badges = $('#badges');
+      progress = response.courses.in_progress;
+      var $completed = $('#completed');
+      var $progress = $('#progress');
       
 
       courses.forEach(function(course) {
 
       	var $course = $('<div />', {
       		'class': 'course'
-      	}).appendTo($badges);
+      	}).appendTo($completed);
 
       	$('<h3 />', {
       		text: course.title
@@ -21,15 +25,37 @@ $(function() {
 
       	$('<img />', {
       		src: course.badge 
-      	})appendTo($course);
+      	}).appendTo($course);
 
       	$('<a />', {
-      		href= course.url,
+      		href: course.url,
       		target: '_blank',
       		'class': 'btn btn-primary',
       		text: 'See course'
       	}).appendTo($course);
-      }
+      })
+
+      progress.forEach(function(course) {
+
+      	var $course = $('<div />', {
+      		'class': 'course'
+      	}).appendTo($progress);
+
+      	$('<h3 />', {
+      		text: course.title
+      	}).appendTo($course);
+
+      	$('<img />', {
+      		src: course.badge 
+      	}).appendTo($course);
+
+      	$('<a />', {
+      		href: course.url,
+      		target: '_blank',
+      		'class': 'btn btn-primary',
+      		text: 'See course'
+      	}).appendTo($course);
+      })
     }
   });
 
